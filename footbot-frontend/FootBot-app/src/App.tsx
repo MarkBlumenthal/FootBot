836320e55bdd2670b89e4686caaee5db1c77c826
Mark Blumenthal <markblumenthal@live.com>
2024-07-08 15:03:31 +0300

    changes

--- footbot-frontend/FootBot-app/src/App.tsx
+++ footbot-frontend/FootBot-app/src/App.tsx
@@ -1,4 +1,4 @@
-// footbot-frontend/FootBot-app/src/App.tsx
+// // footbot-frontend/FootBot-app/src/App.tsx
 import React from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
 import { Home } from './components/Home';
@@ -48,3 +48,16 @@ const App: React.FC = () => {
 };
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
